474951baaee1e6b7df25f2d1cfa5fe0eb9c875da
Miguel Pinto <miguelep0106@gmail.com>
2024-03-18 00:03:30 -0400

    secret key

--- dental_delight/settings.py
+++ dental_delight/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-jee5yvg$3u9)x(2cxb%c5&1gn5ace_0xw3pzsw_q35cl_g+rnf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == "True"
